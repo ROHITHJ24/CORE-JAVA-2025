5cab58d3e73f37bc5308cc89c23713472fccd98b
ROHITHJ24 <johnrohithmidhun1@gmail.com>
2025-07-08 11:55:46 +0530

    Multi level Inheritence

---
+++ MultilevelInheritence.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class A{
+    void displayA(){
+        System.out.println("From A");
+    }
+}
+class B extends A{
+
+    void displayB(){
+        System.out.println("From B");
+    }
+}
+class C extends B{
+
+    void displayC(){
+        System.out.println("From C");
+    }
+}
+
+class MultilevelInheritence{
+    public static void main(String[] args) {
+        C c = new C();
+        c.displayA(); 
+        c.displayB(); 
+        c.displayC();
+    }
+}
