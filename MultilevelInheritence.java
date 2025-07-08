c37e07f632423244aa2bbda66bb3604ae2ffe88d
ROHITHJ24 <johnrohithmidhun1@gmail.com>
2025-07-08 11:57:59 +0530

    Multi level Inheritence

--- MultilevelInheritence.java
+++ MultilevelInheritence.java
@@ -21,7 +21,7 @@ class C extends B{
 class MultilevelInheritence{
     public static void main(String[] args) {
         C c = new C();
-        c.displayA(); multi
+        c.displayA(); 
         c.displayB(); 
         c.displayC();
     }
