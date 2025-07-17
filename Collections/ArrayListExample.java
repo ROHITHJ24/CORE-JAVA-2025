7c9124760afff65a16a957b14d1eaf257889f879
ROHITHJ24 <johnrohithmidhun1@gmail.com>
2025-07-17 16:52:38 +0530

    Collections in java

---
+++ Collections/ArrayListExample.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+class ArrayListExample{
+    public static void main(String[] args) {
+        ArrayList <String> StudentList =new ArrayList <> ();
+        StudentList.add("index 1");
+        StudentList.add("index 2");
+        StudentList.add("index 3");
+        StudentList.add("index 4");
+        StudentList.add("index 5");
+        StudentList.add("index 6");
+        System.out.println(StudentList);
+        System.out.println(StudentList.get(1));
+        System.out.println(StudentList.set(3,"Set method"));
+        System.out.println(StudentList);
+        System.out.println(StudentList.contains("index 1"));
+
+        
+
+    }
+}
